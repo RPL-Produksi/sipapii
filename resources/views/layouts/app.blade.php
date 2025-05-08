2cd8e1fbbbb37c5252bd18c78c3e39fa5966b7b4
Muhamad Hilal <hilal.muhammad0807@gmail.com>
2025-05-08 23:02:34 +0700

    fix: fa not found

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="{{ asset('assets/compiled/css/app.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/compiled/css/app-dark.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/compiled/css/iconly.css') }}" />
-    <link rel="stylesheet" href="https://naramizaru.github.io/fa-pro/css/all.min.css">
+    <link rel="stylesheet" href="https://mizaru.my.id/fa-pro/css/all.min.css">
 
     @stack('css')
 </head>
